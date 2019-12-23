54f123b50fbf015e6c6fe9ec6886b56c43370dcf
Parker Hawke <hawkeboyz2@hotmail.com>
2019-12-23 10:43:11 -0500

    Initial code & foundation commit

---
+++ src/main/java/wtf/choco/strangeplants/StrangePlants.java
@@ -0,0 +1,17 @@
+package wtf.choco.strangeplants;
+
+import net.minecraftforge.fml.common.Mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("strangeplants")
+public class StrangePlants {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public StrangePlants() {
+        
+    }
+
+}
